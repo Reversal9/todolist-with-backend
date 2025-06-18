47517bcb138983d38d936270408076bf8fce556b
Jonnen Chong <jonnenc@gmail.com>
2025-06-18 17:36:32 -0500

    add css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import TodoItem from './TodoItem';
-import AddTodo from './AddTodo';
-import { getTodos, addTodo, updateTodo, deleteTodo } from './API'
+import React, { useEffect, useState } from "react";
+import TodoItem from "./TodoItem";
+import AddTodo from "./AddTodo";
+import { getTodos, addTodo, updateTodo, deleteTodo } from "./API";
+import "./App.css";
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
@@ -12,61 +13,61 @@ const App: React.FC = () => {
 
   const fetchTodos = (): void => {
     getTodos()
-        .then(({data: {todos}}: ITodo[] | any) => {
-            console.log("Setting Todos");
-            setTodos(todos);
-        })
-        .catch((err: Error) => console.log(err));
+      .then(({ data: { todos } }: ITodo[] | any) => {
+        console.log("Setting Todos");
+        setTodos(todos);
+      })
+      .catch((err: Error) => console.log(err));
   };
 
   const handleSaveTodo = (e: React.FormEvent, formData: ITodo): void => {
     e.preventDefault();
     addTodo(formData)
-        .then(({ status, data }) => {
-          if (status !== 201) {
-            throw new Error("Error! Todo not saved");
-          }
-          setTodos(data.todos);
-        })
-        .catch(err => console.log(err));
+      .then(({ status, data }) => {
+        if (status !== 201) {
+          throw new Error("Error! Todo not saved");
+        }
+        setTodos(data.todos);
+      })
+      .catch((err) => console.log(err));
   };
 
   const handleUpdateTodo = (todo: ITodo): void => {
     updateTodo(todo)
-        .then(({ status, data }) => {
-          if (status !== 200) {
-            throw new Error("Error! Todo not updated");
-          }
-          setTodos(data.todos);
-        })
-        .catch(err => console.log(err));
+      .then(({ status, data }) => {
+        if (status !== 200) {
+          throw new Error("Error! Todo not updated");
+        }
+        setTodos(data.todos);
+      })
+      .catch((err) => console.log(err));
   };
 
   const handleDeleteTodo = (_id: string): void => {
     deleteTodo(_id)
-        .then(({ status, data }) => {
-          if (status !== 200) {
-            throw new Error("Error! Todo not deleted");
-          }
-          setTodos(data.todos);
-        })
-        .catch(err => console.log(err));
+      .then(({ status, data }) => {
+        if (status !== 200) {
+          throw new Error("Error! Todo not deleted");
+        }
+        setTodos(data.todos);
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
-      <main className='App'>
-        <h1>My Todos</h1>
-        <AddTodo saveTodo={handleSaveTodo} />
-        {todos.map((todo: ITodo) => (
-            <TodoItem
-                key={todo._id}
-                updateTodo={handleUpdateTodo}
-                deleteTodo={handleDeleteTodo}
-                todo={todo}
-            />
-        ))}
-      </main>
+    <main className="App">
+      <h1>My Todos</h1>
+      <AddTodo saveTodo={handleSaveTodo} />
+      {todos.map((todo: ITodo) => (
+        <TodoItem
+          key={todo._id}
+          updateTodo={handleUpdateTodo}
+          deleteTodo={handleDeleteTodo}
+          todo={todo}
+        />
+      ))}
+    </main>
   );
-}
+};
 
 export default App;
